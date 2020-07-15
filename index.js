693f21719593a8ac697151fca3acdccea3e46bf1
dlwangyilin <dlwangyilin@gmail.com>
2020-07-15 13:10:58 -0700

    Setup jQuery and start html and css.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3001;
 const todoRoutes = require('./routes/todo');
 const bodyParser = require('body-parser');
 
 // Tool for extract data from body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('Root Route');
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
 
 
+const port = 3001;
 app.listen(port, () => {
     console.log('APP is running in ', port);
 });
