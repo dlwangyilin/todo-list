71eaa205c3ae3b654d141c796a795f222022f33e
dlwangyilin <dlwangyilin@gmail.com>
2020-07-15 12:27:22 -0700

    finish 5 rest apis

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const todoRoutes = require('./routes/todo');
+const bodyParser = require('body-parser');
+
+// Tool for extract data from body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Root Route');
--- routes/todo.js
+++ routes/todo.js
@@ -12,4 +12,44 @@ router.get('/', (req, res) => {
         })
 } );
 
+router.post('/', (req, res) => {
+    db.Todo.create(req.body)
+        .then(newTodo => {
+            res.status(201).json(newTodo);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+router.get('/:todoId', (req, res) => {
+    db.Todo.findById(req.params.todoId)
+        .then(foundTodo => {
+            res.json(foundTodo)
+        })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+router.put('/:todoId', (req,res) => {
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+        .then(todo => {
+            res.json(todo);
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
+
+router.delete('/:todoId', (req, res) => {
+    db.Todo.remove({_id: req.params.todoId})
+        .then(() => {
+            res.json({message: 'We delete it'});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
 module.exports = router;
